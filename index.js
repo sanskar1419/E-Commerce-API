f99ccb1a61b4b84e98a8dfce0edc1c17d527ea39
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-01-14 14:50:47 +0530

    Basic authentication for user

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import productRouter from "./src/features/product/product.routes.js";
 import userRouter from "./src/features/user/user.routes.js";
+import basicAuthorizer from "./src/middlewares/basicAuth1.middleware.js";
 
 const app = new express();
 
 app.use(express.json());
 
-app.use("/api/products", productRouter);
+app.use("/api/products", basicAuthorizer, productRouter);
 app.use("/api/users", userRouter);
 
 export default app;
