0d802f3f475d5150d8c788e8902b935d02169bfe
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-01-16 12:39:16 +0530

    Configure CORS Policy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import swagger from "swagger-ui-express";
+import cors from "cors";
 
 import productRouter from "./src/features/product/product.routes.js";
 import userRouter from "./src/features/user/user.routes.js";
@@ -10,6 +11,18 @@ import jwtAuth from "./src/middlewares/jwt.middleware.js";
 import apiDocs from "./swagger.json" assert { type: "json" };
 
 const app = new express();
+
+// CORS policy configuration
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  // return ok for preflight request.
+  if (req.method == "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 
