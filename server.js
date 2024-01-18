bee934955f92e34de5edfebf614db5e7b8153f89
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-01-18 14:38:26 +0530

    Install and connect to mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import app from "./index.js";
+import connectToMongoDB from "./src/config/mongodb.js";
 
 const port = 9000;
 
 app.listen(port, () => {
   console.log(`Server is listening to port :: ${port}`);
+  connectToMongoDB();
 });
